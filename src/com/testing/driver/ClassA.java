3ce38f7b21bae2400936b291f2606852aeb72191
Prasannakumar <prasannacpatil@gmail.com>
2021-08-29 11:45:11 +0530

    initial commit

---
+++ src/com/testing/driver/ClassA.java
@@ -0,0 +1,8 @@
+package com.testing.driver;
+
+public class ClassA {
+
+    public static void main(String[] args) {
+        System.out.println("PK");
+    }
+}
---
+++ src/com/testing/main/Main.java
@@ -0,0 +1,7 @@
+package com.testing.main;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Check");
+    }
+}
